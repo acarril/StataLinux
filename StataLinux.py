9a47e3bf2a335afae4adb6f4809044e246d75370
acarril <acarril@princeton.edu>
2020-02-04 16:11:19 -0500

    allow to execute a selection

--- StataLinux.py
+++ StataLinux.py
@@ -1,32 +1,27 @@
-
 import sublime
 import sublime_plugin
 import os
 
-x = "HALLO"
-# allcontent = sublime.Region(0, self.view.size())
-
 class StataLinuxCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		stataname = "Stata/MP 15.1"
-		region_all = sublime.Region(0, self.view.size())
+		# Current file info:
 		filename = os.path.split(self.view.file_name())[1]
 		filepath = os.path.split(self.view.file_name())[0]
-		# x = self.view.file_name()
-		content = self.view.substr(region_all)
-		# self.view.replace(edit, allcontent, 'Hello, World!')
-		# self.view.insert(edit, 0, lala)
-		# sublime.status_message("Content:%s" % tempfile)
+		# Determine if content is selection and select region accordingly:
+		is_selection = len(self.view.sel()[0]) > 0
+		if is_selection:
+			region = self.view.sel()[0]
+		else:
+			region = sublime.Region(0, self.view.size())
+		content = self.view.substr(region)
+		# Create temporary file with content to be run
 		tempfile = os.path.join(filepath, "tempfile.do")
 		with open(tempfile, "w") as file:
 			file.write(content)
+		# Create and execute bash command:
 		cmd = "sh /home/alvaro/.config/sublime-text-3/Packages/StataLinux/sublime-stata.sh" + " " + '"' + tempfile + '"'
 		os.system(cmd)
+		# Remove temporary file:
 		os.remove(tempfile)
-		# sep
-		# string = ""
-  #       for region in view.sel():
-  #       	if not region.empty():
-  #           	string += view.substr(region)
-  #       if string != "":
-  #           sublime.set_clipboard(string)
+		# Print status message for debugging:
+		# sublime.status_message("Content:%s" % lala)
