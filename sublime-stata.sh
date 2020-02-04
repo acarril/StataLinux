373b2b93a0477ee71c08637d9e51736dd7746ea6
acarril <acarril@princeton.edu>
2020-02-04 16:12:18 -0500

    use xdotool --window option to send directly to Stata window (no win switching)

--- sublime-stata.sh
+++ sublime-stata.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
+# Dependencies: xdotool, xclip
 
-# Packages: wmctrl, xdotool, xclip, xautomation
-# e.g. Fedora: dnf install wmctrl xdotool xclip xautomation
-
-# Copy 'do filename' to clipboard
+# Copy 'do <filename>' to clipboard
 string='do ''"'$1'"'
 echo "${string}" | xclip -selection clipboard
 
-# get current window id
-winid=$(xprop -root | awk '/_NET_ACTIVE_WINDOW\(WINDOW\)/{print $NF}')
-
-# check for Stata window, if found active else execute
-if [ "$(pgrep stata)" ]
-then
-	# Stata Flavor?
-	wmctrl -a 'Stata/MP 15.1'
-	test="1"
-else
-	# If not already open start it
-	xstata-mp
-	test="2"
-	sleep .1
-fi
+# Get relevant windows' IDs
+# winid=$(xprop -root | awk '/_NET_ACTIVE_WINDOW\(WINDOW\)/{print $NF}')
+winid_stata=$(xdotool search --name "Stata/MP 15.1")
 
 # swich to command line and select existing text via ctrl-a and paste clipboard
-sleep .1
 # Note: there is an issue with xdotool's clearmodifiers option (see https://github.com/jordansissel/xdotool/issues/43)
+sleep .1
+
+# OG method:
 # xdotool key --clearmodifiers --delay 50 ctrl+a ctrl+v Return
-xdotool key --delay 50 ctrl+a ctrl+v Return
-sleep .3
+
+# OG method without clearmodifiers option (see note):
+# xdotool key --delay 50 ctrl+a ctrl+v Return
+
+# Separating commands seems to improve OG method:
+# xdotool key --clearmodifiers --delay 50 ctrl+a
+# xdotool key --clearmodifiers --delay 50 ctrl+v
+# xdotool key --clearmodifiers --delay 50 Return
+
+# Current method doesn't switch window:
+# xdotool type --window 48234499 ${string} # xdotool type is slow AF
+xdotool key --window ${winid_stata} --delay 50 ctrl+v Return
+
+sleep .1
 
 # if [ "$test" -eq "2" ]
 #   then
@@ -36,4 +36,4 @@ sleep .3
 # fi
 
 # go back to editor window
-wmctrl -i -a $winid
+# wmctrl -i -a $winid
