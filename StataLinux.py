d42326e2953ec41ba443003e576a8ddc35e1b7ae
Luís Fonseca <luispfonseca@gmail.com>
2020-02-06 10:59:16 +0000

    import path from os

--- StataLinux.py
+++ StataLinux.py
@@ -2,6 +2,7 @@ import sublime
 import sublime_plugin
 import subprocess
 from os import remove
+from os import path
 
 class StataLinuxCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
@@ -9,12 +10,12 @@ class StataLinuxCommand(sublime_plugin.TextCommand):
 		region = self.view.line(self.view.sel()[0])
 		content = self.view.substr(region)
 		# Create temporary file with content to be run:
-		filepath = os.path.split(self.view.file_name())[0]
-		filename = os.path.join(filepath, "tempfile.do")
+		filepath = path.split(self.view.file_name())[0]
+		filename = path.join(filepath, "tempfile.do")
 		with open(filename, "w") as file:
 			file.write(content)
 		# Create and execute bash command:
-		sublime_stata_sh_path = os.path.join(sublime.packages_path(), "StataLinux", "sublime-stata.sh")
+		sublime_stata_sh_path = path.join(sublime.packages_path(), "StataLinux", "sublime-stata.sh")
 		cmd = "sh " + sublime_stata_sh_path + " " + '"' + filename + '"'
 		ret = subprocess.call(cmd, shell = True)
 		if ret != 0:
@@ -29,7 +30,7 @@ class StataLinuxAllCommand(sublime_plugin.TextCommand):
 		# Define current file as the one to be run:
 		filename = self.view.file_name()
 		# Create and execute bash command:
-		sublime_stata_sh_path = os.path.join(sublime.packages_path(), "StataLinux", "sublime-stata.sh")
+		sublime_stata_sh_path = path.join(sublime.packages_path(), "StataLinux", "sublime-stata.sh")
 		cmd = "sh " + sublime_stata_sh_path + " " + '"' + filename + '"'
 		ret = subprocess.call(cmd, shell = True)
 		if ret != 0:
